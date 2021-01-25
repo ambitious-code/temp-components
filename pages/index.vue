ad2d17846cc5494bfb7e9162c5c1a4a18e38e6cb
Wes Wilson <wes@iamweswilson.com>
2021-01-24 16:27:17 -0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Components</h1>
+    <ul>
+      <li>
+        <a href="/pages/sample-one">Sample number one</a>
+      </li>
+      <li>
+        <a href="/pages/sample-two">Sample number two</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 1.875rem;
+    font-weight: 600;
+  }
+  a {
+    color: #3182ce;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
