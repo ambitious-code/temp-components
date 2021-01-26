86047f2c0741dee895037a047be4037764b62f95
Wes Wilson <wes@iamweswilson.com>
2021-01-25 16:48:35 -0800

    Start of sample marketing page

---
+++ pages/sample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Features class="my-32"/>
+    <Contact class="my-32"/>
+  </div>
+</template>
+
+<script>
+import Contact from './components/Contact.vue'
+import Features from './components/Features.vue'
+  export default {
+    components: { 
+      Features,
+      Contact
+    },
+  }
+  </script>
+
+<style lang="scss" scoped>
+
+</style>
