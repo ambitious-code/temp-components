c4d37c273cc030772b0dda342a4f5a91ae1cf133
Wes Wilson <wes@iamweswilson.com>
2021-01-25 16:48:17 -0800

    using .component-group across all components

--- pages/components/Features.vue
+++ pages/components/Features.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <section class="my-8 selector">
+  <div class="component-group">
+    <section class="selector">
       <button @click="changeFeature('two')" class="bg-gray-200 p-2 rounded" :class="{active: activeFeature == 'two'}">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" />
